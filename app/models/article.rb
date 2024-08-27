1de6203d73c8e84a950a43c73ad768384d8dac11
Amin Zare <119476867+AminEdZare@users.noreply.github.com>
2024-08-27 19:02:22 -0400

    added cross page functionality, add features, accessibility, and interactive UI

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
   include Visible
 
   has_one_attached :coffee_cup do |attachable|
-    attachable.variant :thumb, resize_to_limit: [100, 100]
+    attachable.variant :thumb, resize_to_limit: [400, 400]
   end
   has_many :comments, dependent: :destroy
 
